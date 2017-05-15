37afcc533106328c07b3d2b94da6565a2c224de4
PrabodhaNirmani <prabodhanh@gmail.com>
2017-05-15 17:22:40 +0530

    heroku deployed

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express=require('express');
 var bodyParser=require('body-parser');
 var mongoose=require('mongoose');
 var config=require('./config.json');
+var path=require('path');
 
 var app=express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -21,6 +22,11 @@ mongoose.Promise=global.Promise;
 // mongodb://127.0.0.1:27017/online_blood_bank
 mongoose.connect(config.database);
 app.use(require('./routes'));
+
+app.get('*',function(req,res){
+	res.sendFile(path.join(__dirname+'/index.html'));
+})
+
 
 http.createServer(app).listen(port,function(err){
 	console.log("connected to the port"+port);
